b819444250f4d32baf2c9d67be26df93f03001da
Dilan <148026153+dilandz01@users.noreply.github.com>
2025-02-18 11:18:33 +0530

    comment updated

--- src/components/ChatbotWidgetComponent.js
+++ src/components/ChatbotWidgetComponent.js
@@ -13,8 +13,8 @@ class ChatbotWidgetElement extends HTMLElement {
     // Create a <style> element to add CSS inside Shadow DOM
     const style = document.createElement("style");
     style.textContent = `
-      @import url("https://ow-ai-chatbot.netlify.app/ow-chatbot.css"); // frontend deployed link for css file
-    `;
+      @import url("https://ow-ai-chatbot.netlify.app/ow-chatbot.css"); 
+    `;// frontend deployed link for css file
 
     // Check if already rendered
     if (!this.shadowRoot.innerHTML) {
